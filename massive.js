8471540b114818d13f83efae79633c1effbeaa05
Kalavrat <97897883+Kalavrat@users.noreply.github.com>
2022-02-10 12:05:34 +0300

    Add files via upload

---
+++ massive.js
@@ -0,0 +1,136 @@
+// //Styles
+// styles = ["Джас", "Блюз"]
+
+// styles.push("Рок-н-ролл")
+
+// console.log(styles)
+// if (styles.length % 2 == 0)
+// {
+//     let i = styles.length / 2
+//     styles[i] = "Классика"
+// } else{
+//     let i = Math.floor(styles.length / 2)
+//     styles[i] = "Классика"
+// }
+// console.log(styles)
+
+// console.log(styles.shift())
+// console.log(styles)
+
+// styles.unshift("Рэп")
+// styles.unshift("Регги")
+// console.log(styles)
+
+
+// //1 Задание
+// for (let i = 1; i < 101; i ++)
+// {
+//     console.log(i)
+// }
+
+
+// //2 Задание
+// for (let i = 100; i > 0; i --)
+// {
+//     console.log(i)
+// }
+
+
+// //3 Задание
+// for (let i = 1; i < 101; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+// //4 Задание
+// let mass = []
+// for (let i = 0; i < 10; i++)
+// {
+//     mass.push("x")
+// }
+// console.log(mass)
+
+
+// //5 Задание
+// mass = []
+// for (let i = 1; i < 11; i ++)
+// {
+//     mass.push(i)
+// }
+// console.log(mass)
+
+
+// //6 Задание
+// mass = []
+// for (i = 0; i < 10; i ++)
+// {
+//     mass.push(Math.random().toFixed(2))
+// }
+// console.log(mass)
+
+
+// //7 Задание
+// mass = []
+// for (let i = 0; i < 10; i ++)
+// {
+//     mass.push(Math.round(Math.random()*9 + 1))
+// }
+// console.log(mass)
+
+
+// //8 Задание 
+// mass = [4, 1, -3, 0, -10, 90, 404, 24, 2, 9, -9]
+// for (let i = 0; i < mass.length; i ++)
+// {
+//     if (mass[i] > 0 && mass[i] < 10)
+//     {
+//         console.log(mass[i])
+//     }
+// }
+
+
+// //9 Задание
+// mass = [4, 1, -3, 5, -10, 90, 404, 5, , 9, -9]
+// for (let i = 0; i < mass.length; i ++)
+// {
+//     if (mass[i] == 5)
+//     {
+//         console.log("Есть")
+//         break
+//     }
+// }
+
+
+// //10 Задание
+// mass = [4, 1, -3, 0, -10, 90, 404, 24, 2, 9, -9]
+// sum = 0
+// for (let i = 0; i < mass.length; i ++)
+// {
+//     sum += mass[i]
+// }
+// console.log(sum)
+
+
+// //11 Задание
+// mass = [1, 8, 2, -2, 0]
+// sum = 0
+// for (let i = 0; i < mass.length; i ++)
+// {
+//     sum += Math.pow(mass[i], 2)
+// }
+// console.log(sum)
+
+
+// //12 Задание
+// mass = [1, 3, 9, 0, -1, -30, 20]
+// sum = 0
+// for (let i = 0; i < mass.length; i ++)
+// {
+//     sum += mass[i]
+// }
+// sum /= mass.length
+// console.log(sum)
